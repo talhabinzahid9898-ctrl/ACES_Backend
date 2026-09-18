9ae7e9c18ac7dbd974603d613c4ffc8d825b79ef
talhabinzahid9898-ctrl <talhabinzahid9898@gmail.com>
2026-09-18 12:27:36 +0500

    Testing phase (error resolved in backend)

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ require("dotenv").config();
 const { connectDB } = require("./Config/db");
 
 const app = express();
+const cors = require("cors");
+app.use(cors({ origin: "https://verdant-salmiakki-3cb05c.netlify.app/", credentials: true }));
 const PORT = 3000;
 
 
